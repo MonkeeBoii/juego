d1ab3fee694256b94f36f9e5816d782a09537231
MonkeeBoii <alvarolop.alumn@cescristorey.com>
2024-02-15 12:31:22 +0100

    v0.1

---
+++ marioBros/core/src/com/monkee/mario/Screens/PlayScreen.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.monkee.mario.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.monkee.mario.MarioBros;
+
+/**
+ *
+ * @author monkeeboi
+ */
+public class PlayScreen implements Screen {
+    private MarioBros game;
+    Texture texture;
+    private OrthographicCamera camera;
+    private Viewport view;
+    
+    public PlayScreen(MarioBros game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        camera = new OrthographicCamera();
+        view = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, camera);
+    }
+    
+    @Override
+    public void show() {
+       
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        view.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+    
+}
